f9f34b05548ae696e11f223cb3f994e4a2d2d73b
Joel Hiltunen <84034458+jolbertti@users.noreply.github.com>
2022-10-08 00:16:34 +0300

    Add files via upload

---
+++ uarttitesti.py
@@ -0,0 +1,8 @@
+from machine import UART, Pin
+from time import sleep_us
+
+uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1), bits=8, parity=None, stop=1)
+
+uart.write("AT\r\n")
+sleep_us(100000)
+print(uart.read())
