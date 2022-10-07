20aff9ba4619ac17202a879716b373df89df4d0d
Joel Hiltunen <84034458+jolbertti@users.noreply.github.com>
2022-10-08 00:17:52 +0300

    Update uarttitesti.py

--- uarttitesti.py
+++ uarttitesti.py
@@ -3,6 +3,6 @@ from time import sleep_us
 
 uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1), bits=8, parity=None, stop=1)
 
-uart.write("AT\r\n")
+uart.write("AT\r\n") # returns OK if it works - search AT commands for more
 sleep_us(100000)
 print(uart.read())
